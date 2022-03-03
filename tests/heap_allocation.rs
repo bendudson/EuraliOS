d0f2b5962f7bf3fe0c75422465766d03aef0de9e
Ben Dudson <dudson2@llnl.gov>
2022-03-02 22:26:39 -0800

    Step 11a : Add many_boxes_long_lived test
    Not adding more allocators (yet), just another test
    https://os.phil-opp.com/allocator-designs/

--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -68,3 +68,13 @@ fn many_boxes() {
         assert_eq!(*x, i);
     }
 }
+
+#[test_case]
+fn many_boxes_long_lived() {
+    let long_lived = Box::new(1); // A single long-lived allocation
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i); // Many short-lived allocations
+        assert_eq!(*x, i);
+    }
+    assert_eq!(*long_lived, 1); // new
+}
