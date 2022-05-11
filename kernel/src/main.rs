6d2d635f04b4d0b103036702cb6c004fc80d718d
Ben Dudson <dudson2@llnl.gov>
2022-05-10 22:30:45 -0700

    Change to Cargo Workspace
    Kernel and user-space "hello" executable are now separate packages,
    and the user-space code no longer links against the kernel lib code.
    
    Changed how linker flags are passed, now using a build.rs file rather
    than compiler flags in the makefile.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -5,12 +5,12 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use blog_os::println;
+use kernel::println;
 use bootloader::{BootInfo, entry_point};
 
-use blog_os::memory;
-use blog_os::syscalls;
-use blog_os::process;
+use kernel::memory;
+use kernel::syscalls;
+use kernel::process;
 
 entry_point!(kernel_entry);
 
@@ -23,9 +23,9 @@ fn kernel_thread_main() {
 
     // Using MOROS approach of including ELF files
     // https://github.com/vinc/moros/blob/trunk/src/usr/install.rs
-    process::new_user_thread(include_bytes!("../user/hello"));
+    process::new_user_thread(include_bytes!("../../user/hello"));
 
-    blog_os::hlt_loop();
+    kernel::hlt_loop();
 }
 
 
@@ -36,7 +36,7 @@ fn kernel_thread_main() {
 ///  BootInfo    Bootloader memory mapping information
 ///
 fn kernel_entry(boot_info: &'static BootInfo) -> ! {
-    blog_os::init();
+    kernel::init();
 
     // Set up memory and kernel heap with allocator
     memory::init(boot_info);
@@ -51,7 +51,7 @@ fn kernel_entry(boot_info: &'static BootInfo) -> ! {
     // which will be scheduled and take over from here
     process::new_kernel_thread(kernel_thread_main);
 
-    blog_os::hlt_loop();
+    kernel::hlt_loop();
 }
 
 /// This function is called on panic.
@@ -59,13 +59,13 @@ fn kernel_entry(boot_info: &'static BootInfo) -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    blog_os::hlt_loop();
+    kernel::hlt_loop();
 }
 
 #[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    blog_os::test_panic_handler(info)
+    kernel::test_panic_handler(info)
 }
 
 #[test_case]
--- makefile
+++ makefile
@@ -1,16 +1,11 @@
 
-.PHONY: user run
-
-# Compile user programs in src/bin
-user: user/hello
-
-user/% : src/bin/%.rs makefile
-	cargo rustc --release --bin $* -- \
-		-C linker-flavor=ld \
-		-C link-args="-Ttext-segment=5000000 -Trodata-segment=5100000" \
-		-C relocation-model=static
+user/% : FORCE
+	cargo build --release --bin $*
 	mkdir -p user
-	cp target/x86_64-blog_os/release/$* user/
+	cp target/x86_64-euralios/release/$* user/
 
-run : user
-	cargo run --bin blog_os
+FORCE:
+
+.PHONY: run
+run : user/hello
+	cargo run --release --bin kernel
